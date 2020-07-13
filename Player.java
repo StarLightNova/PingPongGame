f273b8333676c3f5209ed823ebe97e8c7a86ac90
Alikhan Toleubay <52784852+StarLightNova@users.noreply.github.com>
2020-07-13 20:14:24 +0600

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,23 +1,7 @@
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-
-public class Player extends Pane{
-	private long score;
-	private Pane pane;
-	private Rectangle shape;
-	private double X;
-	private double Y;
-
-	public Player(int x, int y, int w, int h){
-		pane = new Pane();
-		shape = new Rectangle(x, y, w, h);
-		shape.setFill(Color.WHITE);
-		pane.getChildren().add(shape);
-		getChildren().add(pane);
-	}
-
+abstract class Player extends Pane{
+	protected int score = 0;
+	abstract int getScore();
+	abstract void setScore(int other);
+	abstract void addScore();
 }
