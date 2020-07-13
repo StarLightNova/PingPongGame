87fb47fddbc2d0ff0bfa2123b1ded36290c0165b
Alikhan Toleubay <52784852+StarLightNova@users.noreply.github.com>
2020-07-13 20:15:12 +0600

    Update BoardPane.java

--- BoardPane.java
+++ BoardPane.java
@@ -6,17 +6,23 @@ import javafx.scene.layout.Pane;
 public class BoardPane extends Pane{
 	private Double Window_W = 640.00;	//Standart Window Resolutions Height and Width
 	private Double Window_H = 480.00;
-	BallPane ball;
-	Pane primaryPane;
+	private BallPane ball;
+	private Pane primaryPane;
+	private MyPlayer leftPlayer, rightPlayer;
 
 	public BoardPane(){
 		this.primaryPane = new Pane();
 		this.ball = new BallPane();
+
+		this.leftPlayer = new MyPlayer(this.Window_W / 4, this.Window_H / 2);
+		this.rightPlayer = new MyPlayer(this.Window_W - this.Window_W / 4 + this.leftPlayer.getMyPlayerCenter() + this.ball.getMyCenter() * 2 , this.Window_H / 2);
+
 		this.ball.setTranslateX(this.Window_W / 2 - this.ball.getMyCenter()); // Move to the center
 		this.ball.setTranslateY(this.Window_H / 2 - this.ball.getMyCenter());
 		this.primaryPane.setPrefSize(this.Window_W, this.Window_H);	//Set Screen to Standart
 		this.primaryPane.setStyle("-fx-background-color: BLACK");	//Change pane background
-		this.primaryPane.getChildren().add(this.ball);					//Add ball to board
+		
+		this.primaryPane.getChildren().addAll(this.ball, this.leftPlayer, this.rightPlayer);	//Add ball and Players to the board
 		getChildren().add(this.primaryPane);
 	}
 
