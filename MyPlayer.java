b507a3cd5dfa8afabcae7f20166fbd97270ba7ac
Alikhan Toleubay <52784852+StarLightNova@users.noreply.github.com>
2020-07-14 18:10:27 +0600

    Update MyPlayer.java

--- MyPlayer.java
+++ MyPlayer.java
@@ -5,6 +5,7 @@ class MyPlayer extends Player implements IMoveable{
 	private Double Player_W = 25.00;
 	private Double Player_H = 100.00;
 	private Double Center = Math.sqrt(Math.pow(this.Player_H, 2) + Math.pow(this.Player_W, 2)) / 2;
+	private Double speed = 25.25;
 
 	public MyPlayer(Double x, Double y){
 		this.primary = new Pane();
@@ -34,10 +35,42 @@ class MyPlayer extends Player implements IMoveable{
 		this.score++;
 	}
 
-	@Override
-	public void moveUp(){}
+	public void setSpeed(Double speed){
+		this.speed = speed;
+	}
+
+	public Double getSpeed(){
+		return this.speed;
+	}
+
+	public Double getMyPlayerH(){
+		return this.Player_H;
+	}
+
+	public Double getMyPlayerW(){
+		return this.Player_W;
+	}
+
+	public Double getMyYCoor(){
+		return this.primary.getTranslateY();
+	}
 
 	@Override
-	public void moveDown(){}
+	public void moveUp(){
+		this.primary.setTranslateY(this.primary.getTranslateY() - this.speed);
+	}
+
+	@Override
+	public void moveDown(){
+		this.primary.setTranslateY(this.primary.getTranslateY() + this.speed);
+	}
+
+	public void TranslateToZero(){
+		this.primary.setTranslateY(0);
+	}
+
+	public void TranslateToWindowHeight(Double height){
+		this.primary.setTranslateY(height);
+	}
 
 }
